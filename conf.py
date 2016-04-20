d97f9125ec67ff604a3663248ba371c22803551b
Yaroslav <123kapyar@gmail.com>
2016-04-20 15:03:37 +0300

    add theme

--- conf.py
+++ conf.py
@@ -29,6 +29,12 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme"
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
